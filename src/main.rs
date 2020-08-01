25a9737315f43d769460827b13704f3e3edf1324
Will Hawkins <hawkinsw@obs.cr>
2020-08-01 03:11:02 -0400

    Code cleanup.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
 fn main() {
-    let f: f32 = 6.2f32;
-    let mut f_i32 = i32::from_be_bytes(f.to_be_bytes());
+    let mut f_i32 = i32::from_be_bytes(6.2f32.to_be_bytes());
+    let mut mantissa:String = format!("");
 
     let sign = if f_i32 < 0 {
         format!("1")
     } else {
         format!("0")
     };
+
+    // shift off the sign bit.
     f_i32<<=1;
 
     let exponent = f_i32.to_be_bytes()[0];
 
+    // shift off the exponent bits.
     f_i32<<=8;
 
-    println!("sign: {}", sign);
-    println!("exponent: {:b}", exponent);
-    print!("mantissa: ");
-    for i in 0..23 {
+    for _ in 0..23 {
         if f_i32<0 {
-            print!("1");
+            mantissa += &format!("1")
         } else {
-            print!("0");
+            mantissa += &format!("0");
         }
         f_i32<<=1;
     }
-    println!("");
+
+    println!("{}/{:b}/{}", sign, exponent, mantissa);
 }
