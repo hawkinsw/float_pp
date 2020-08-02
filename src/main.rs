0db3f2f2884edabd57f09612b2500bc4a3f6be24
Will Hawkins <hawkinsw@obs.cr>
2020-08-02 09:46:28 -0400

    cargo fmt and some other examples.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::fmt;
 use std::fmt::Display;
 use std::fmt::Formatter;
-use std::fmt;
 
 struct PrettyPrintableFloat {
     f: f32,
@@ -28,17 +28,13 @@ impl Display for PrettyPrintableFloat {
 
         // Create a boolean to hold whether the value printed is
         // negative or positive.
-        let is_negative = if f_bits < 0 {
-            true
-        } else {
-            false
-        };
+        let is_negative = if f_bits < 0 { true } else { false };
 
         // shift off the sign bit.
         f_bits <<= 1;
 
         // Create a variable to hold the exponent. This is biased
-        // by 127 in ieee754 to account for that, too. Also, cast 
+        // by 127 in ieee754 to account for that, too. Also, cast
         // to u32 so that we can use it in a for loop later.
         let exponent = (f_bits.to_be_bytes()[0] - 127) as u32;
 
@@ -49,7 +45,7 @@ impl Display for PrettyPrintableFloat {
         // For every 0..exponent bits of the bits in the mantissa,
         // shift them into the pre radix point. From above, the pre
         // radix point already contains the implicit 1. So, all we
-        // have to do at each step is 
+        // have to do at each step is
         // 1. shift the pre radix point binary representation to the left
         // by one (This is from the shortcut algorithm to convert
         // a binary digit to a decimal digit.)
@@ -77,12 +73,12 @@ impl Display for PrettyPrintableFloat {
         // The first bit after the radix point represents 1/2^1.
         // The second bit after the radix point represents 1/2^2.
         // The third bit after the radix point represents 1/2^3, etc.
-        for i in 0..(23-exponent) {
+        for i in 0..(23 - exponent) {
             if f_bits < 0 {
                 mantissa += &format!("1");
                 // Because we are counting from 0 in the for loop, we have
                 // to add 1 to i in our calculation here (see above for why).
-                post_radix_point += 1.0f64 / (2u64.pow(i+1) as f64);
+                post_radix_point += 1.0f64 / (2u64.pow(i + 1) as f64);
             } else {
                 mantissa += &format!("0");
             }
@@ -90,20 +86,35 @@ impl Display for PrettyPrintableFloat {
         }
 
         // Do the printing.
-        let mut result = write!(f, "{}\n", ((pre_radix_point as f64) + post_radix_point)*(if is_negative { -1f64 } else { 1f64}));
+        let mut result = write!(
+            f,
+            "{}\n",
+            ((pre_radix_point as f64) + post_radix_point)
+                * (if is_negative { -1f64 } else { 1f64 })
+        );
         result = write!(f, "S/Exponent/Mantissa:\n");
-        result = write!(f, "{}/{:8b}/{}", if is_negative { "1" } else { "0" }, exponent, mantissa);
+        result = write!(
+            f,
+            "{}/{:8b}/{}",
+            if is_negative { "1" } else { "0" },
+            exponent,
+            mantissa
+        );
         result
     }
 }
 
 impl PrettyPrintableFloat {
     fn new(f: f32) -> Self {
-        Self{f}
+        Self { f }
     }
 }
 
 fn main() {
-    let pp = PrettyPrintableFloat::new(-9.4);
+    let mut pp = PrettyPrintableFloat::new(-9.4);
+    println!("{}", pp);
+    pp = PrettyPrintableFloat::new(9.5);
+    println!("{}", pp);
+    pp = PrettyPrintableFloat::new(10.3);
     println!("{}", pp);
 }
